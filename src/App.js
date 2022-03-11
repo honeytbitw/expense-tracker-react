31459c7476de21d611d9490acf6e7ba15c2b3822
honeytbitw <vedanttbitw@gmail.com>
2022-03-11 12:24:49 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Splitwise from './Components/Splitwise';
+import React from 'react'
+import { Expensetracker } from './Components/Expensetracker';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Registration from './Components/Registration';
+import Login from './Components/Login';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+
+      <Route path="/register">
+        <Registration/>
+        </Route>
+    
+        <Route path="/login">
+        <Login/>
+        </Route>
+
+      <Route path="/expense">
+        <Expensetracker/>
+        </Route>
+        
+      <Route path="/split">
+        <Splitwise/>
+        </Route>
+      </Switch>
+      {/* <Splitwise/> */}
+      {/* <Expensetracker/> */}
     </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Login.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { Alert } from "react-bootstrap";
+// import Home from "./Home";
+
+// // import { Header } from './Header';
+// // import { Balance } from './Balance';
+// // import { IncomeExpenses } from './IncomeExpenses';
+// // import { TransactionList } from './TransactionList';
+// // import { AddTransaction } from './AddTransaction';
+// // import {Splitwise} from './Splitwise'
+// // import {Graph} from 'react-d3-graph'
+// import { GlobalProvider } from './GlobalState';
+function Login() {
+  const [emaillog, setEmaillog] = useState(" ");
+  const [passwordlog, setPasswordlog] = useState(" ");
+
+  const [flag, setFlag] = useState(false);
+
+  const [home, setHome] = useState(true);
+
+  function handleLogin(e) {
+    e.preventDefault();
+    let pass = localStorage
+      .getItem("sanskarPassword")
+      .replace(/"/g, "");
+    let mail = localStorage.getItem("sanskarEmail").replace(/"/g, "");
+    
+
+    if (!emaillog || !passwordlog) {
+      setFlag(true);
+      console.log("EMPTY");
+    } 
+    else if (passwordlog !== pass || emaillog !== mail) {
+      setFlag(true);
+    } 
+    else {
+      setHome(!home);
+      setFlag(false);
+    }
+  }
+
+  return (
+    <div>
+      {home ? (
+        <form onSubmit={handleLogin}>
+          <h3>Log In</h3>
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              onChange={(event) => setEmaillog(event.target.value)}
+            />
+          </div>
+        <br></br>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              onChange={(event) => setPasswordlog(event.target.value)}
+            />
+          </div>
+          <br></br>
+          <button type="submit" className="btn btn-dark btn-lg btn-block">
+            Login
+          </button>
+
+          {flag && (
+            <Alert color="primary" variant="warning">
+              Fill correct info else keep trying.
+            </Alert>
+          )}
+        </form>
+      ) : (
+        //  <Splitwise />
+
+       <></>
+      )}
+    </div>
+  );
+}
+
+export default Login;
